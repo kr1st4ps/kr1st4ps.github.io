a172f1e034de2e9684f756aedb0735526de3081a
Kristaps Ālmanis <kristapsalmanis@Kristapss-MacBook-Air-2.local>
2023-02-06 22:00:58 +0200

    Fixed json for games-deal-tracker project

---
+++ Data/json_modifier.py
@@ -0,0 +1,33 @@
+import json
+import sys
+
+name = sys.argv[1]
+action = sys.argv[2]
+console = sys.argv[3]
+
+with open("games.json", "r") as file:
+    games = json.load(file)
+
+console_games = games[console]
+if action == "remove":
+    for i in range(len(console_games)):
+        if console_games[i]["name"] == name:
+            console_games.pop(i)
+elif action == "add":
+    if console == "PS4":
+        new_data = {
+            "name": name,
+            "price": None,
+            "price plus": None,
+            "best price": None
+        }
+    else:
+        new_data = {
+            "name": name,
+            "price": None,
+            "best price": None
+        }
+    console_games.append(new_data)
+
+with open("games.json", "w") as file:
+    json.dump(games, file, indent=2, separators=(',', ': '))
